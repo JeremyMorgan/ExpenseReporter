ab72d27ef489c76e612a95caa3d8bc62626999f1
Jeremy Morgan <jeremymorgan@gmail.com>
2015-09-09 12:44:56 -0700

    initial commit

---
+++ src/app/Expense/testCtrl.js
@@ -0,0 +1,13 @@
+/**
+ * @ngdoc controller
+ * @name test
+ *
+ * @description
+ * _Please update the description and dependencies._
+ *
+ * @requires $scope
+ * */
+angular.module('revokinatorApp')
+    .controller('test', function($scope){
+
+});
